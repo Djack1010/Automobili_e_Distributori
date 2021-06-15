5129df40123715e61a895f1681f8b4a3c1743916
ImLearningJava <85734828+ImLearningJava@users.noreply.github.com>
2021-06-15 15:03:33 +0200

    Update Wheels.java

--- Wheels.java
+++ Wheels.java
@@ -1,11 +1,18 @@
+/***********************************************
+*	Paolo Bondi - Corso TIC (UF6) - 04/06/21   *
+*	Esercizio2 - Automobili e distributori v2  *											
+***********************************************/
+
 public class Wheels {
   
   private float statoRuote; // km percorsi per valutare l'usura delle ruote
   private float pressioneRuote;
+  private float maxP;
   
   public Wheels(float maxPressione) {
     statoRuote = 0;
     pressioneRuote = maxPressione; // inizializzo ruote gonfie al massimo, 2.5 bar
+    maxP = maxPressione;
   }
   
   public float getStatoRuote() { return statoRuote; }
@@ -18,26 +25,34 @@ public class Wheels {
       pressioneRuote = 0.01f; 
   }
     
-  public void controlloRuote(){
+  public int controlloRuote(){
   
   	if (statoRuote > 300 && pressioneRuote < 1.5) {
-  	  System.out.println("Necessario controllare e gonfiare le ruote");
-  	  return;
+  	  return 3;	// Necessario controllare e gonfiare le ruote
   	}
   	
   	if (statoRuote > 300) {
-  	  System.out.println("Necessario controllare le ruote");
-  	  return;
+  	  return 1; // Necessario controllare le ruote
   	}
   	
   	if (pressioneRuote < 1.5){
-  	  System.out.println("Necessario gonfiare le ruote");
-  	  return;
+  	  return 2; // Necessario gonfiare le ruote
   	}
-  	
-  	System.out.println("Ruote in perfetto stato, nessuna operazione richiesta");
+  	  return 0; // Ruote in perfetto stato, nessuna operazione richiesta
+  }
+     
+  public String stampaInfo() { return "statoRuote (" + statoRuote + "), pressioneRuote (" + 
+		  							   pressioneRuote + "), statoMacchina (" + controlloRuote()+")" ; }
+  
+  
+  public void setAzzeraRuote() {
+	  statoRuote = 0;
+	  System.out.println("*** Lo stato delle ruote è stato ripristinato.");
   }
   
-  public String stampaInfo() { return "statoRuote (" + statoRuote + "), pressioneRuote (" + pressioneRuote + ")"; }
-  
+  public void setMaxPressioneRuote() {
+	  pressioneRuote = maxP;
+	  System.out.println("*** La ruote sono state gonfiate.");
+  }
+   
 }
