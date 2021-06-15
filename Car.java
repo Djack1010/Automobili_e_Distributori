024ada9890661b80b855e2c50368eafb74f0c041
ImLearningJava <85734828+ImLearningJava@users.noreply.github.com>
2021-06-15 15:03:09 +0200

    Update Car.java

--- Car.java
+++ Car.java
@@ -1,22 +1,32 @@
+/***********************************************
+*	Paolo Bondi - Corso TIC (UF6) - 04/06/21   *
+*	Esercizio2 - Automobili e distributori v2  *											
+***********************************************/
+
 public class Car 
 {
-  // TODO: aggiungere anche la capacit√† massima del serbatorio, e implementare controlli per assicurarsi che il gas immagazzinato non ecceda la capacit√† massima
+  // TODO: aggiungere anche la capacit‡ massima del serbatorio, e implementare controlli 
+  // per assicurarsi che il gas immagazzinato non ecceda la capacit‡ massima
+  // FATTO
+	
   private String nome;
   private float resa;
   private float serbatoio;
   private Wheels ruote;
   
   private float MAX_PRESSIONE = 2.5f;
+  private float MAX_CAPACITA_SERBATOIO = 50f;
 
-  public Car(String n, float r) {
+    public Car(String n, float r) {
     nome = n;
     resa = r;
     serbatoio = 0;
-    ruote = new Wheels(MAX_PRESSIONE);
+    setRuote(new Wheels(MAX_PRESSIONE));
+    
   }
 
   public float getGas() { return serbatoio; }
-
+  
   public void setGas(float qta) { serbatoio += qta; }
 
   public void drive(float percorso) {
@@ -24,23 +34,46 @@ public class Car
     float resaReale = 0;
   	  
     for(int i=0; i<percorso; i++) {
-      resaReale = (ruote.getPressioneRuote() * motore.getResa()) / MAX_PRESSIONE;
+      resaReale = (getRuote().getPressioneRuote() * resa) / MAX_PRESSIONE;
       serbatoio = serbatoio - (1 / resaReale);
-      ruote.setStatoRuote(1);
-      ruote.setPressioneRuote(1);
+      
+      if(serbatoio < 0) {			// se il serbatoio va sottozero
+    	  serbatoio = 0;			// si azzera il serbatoio
+    	  break;					// e si forza l'uscita dal ciclo
+    	  }
+            
+      getRuote().setStatoRuote(1);
+      getRuote().setPressioneRuote(1);
     }
-    
-  }
+   }
   
   // TODO: refactor controlloRuote(): il metodo deve restituire un intero
   //   0 -> se tutto ok
   //   1 -> se vanno controllate le ruote
   //   2 -> se vanno gonfiate le ruote
   //   3 -> se vanno sia controllate che gonfiate
-  public void controlloRuote() { ruote.controlloRuote(); } 
+  // FATTO
+  
+  public void controlloRuote() { 
+	System.out.println(getRuote().controlloRuote()); 
+	} 
   
   public void stampaInfo(){
-    System.out.println("serbatoio (" + serbatoio + "), " + ruote.stampaInfo());
+    System.out.println("serbatoio (" + serbatoio + "), " + getRuote().stampaInfo());
+    }
+  
+  public float serbatoioDisponibile() {
+	  return MAX_CAPACITA_SERBATOIO - serbatoio;
+	  
   }
 
+public Wheels getRuote() {
+	return ruote;
 }
+
+public void setRuote(Wheels ruote) {
+	this.ruote = ruote;
+}
+
+}
+
