c1ef67bc15e9418309e9b4cb2deea46b3d7adf5a
cirodelia74 <85739966+cirodelia74@users.noreply.github.com>
2021-06-15 15:17:46 +0200

    Update Car.java

--- Car.java
+++ Car.java
@@ -1,46 +1,92 @@
+/*
+ * author: Ciro
+ */
 public class Car 
 {
-  // TODO: aggiungere anche la capacità massima del serbatorio, e implementare controlli per assicurarsi che il gas immagazzinato non ecceda la capacità massima
   private String nome;
   private float resa;
   private float serbatoio;
+  private float maxSerbatoio; // aggiunta capacita' massima del serbatoio
   private Wheels ruote;
-  
-  private float MAX_PRESSIONE = 2.5f;
 
-  public Car(String n, float r) {
+  private float MAX_PRESSIONE = 2.5f; // assegnamo di default una pressione massima
+
+  // Costruttore
+  public Car(String n, float r, float max) {
     nome = n;
     resa = r;
     serbatoio = 0;
+    maxSerbatoio = max;
     ruote = new Wheels(MAX_PRESSIONE);
   }
-
-  public float getGas() { return serbatoio; }
-
-  public void setGas(float qta) { serbatoio += qta; }
+  
+  public float setGas(float qta) { 
+	   
+    	if (qta <= (maxSerbatoio - serbatoio)) {
+    		serbatoio += qta;
+    		return 0;
+    	}
+    	else {
+    		float reso = qta - (maxSerbatoio - serbatoio);
+    		serbatoio = maxSerbatoio;
+    		return reso;
+    	}
+  }
 
   public void drive(float percorso) {
-  
+    
     float resaReale = 0;
-  	  
-    for(int i=0; i<percorso; i++) {
-      resaReale = (ruote.getPressioneRuote() * motore.getResa()) / MAX_PRESSIONE;
+    
+  	for(int i=0; i<percorso; i++) {
+      resaReale = (ruote.getPressioneRuote() * resa) / MAX_PRESSIONE;
       serbatoio = serbatoio - (1 / resaReale);
       ruote.setStatoRuote(1);
       ruote.setPressioneRuote(1);
+      if (serbatoio < 0) {
+    	  serbatoio = 0; // Forzo il serbatoio a VUOTO se andato in negativo
+    	  return;
+      }
     }
-    
   }
   
-  // TODO: refactor controlloRuote(): il metodo deve restituire un intero
-  //   0 -> se tutto ok
-  //   1 -> se vanno controllate le ruote
-  //   2 -> se vanno gonfiate le ruote
-  //   3 -> se vanno sia controllate che gonfiate
-  public void controlloRuote() { ruote.controlloRuote(); } 
+  // Il metodo sfrutta quello della classe Wheels
+  public int controlloRuote() { 
+	  return ruote.controlloRuote(); 
+  } 
+  
+  public float getGas() { 
+	  return serbatoio;
+  }
   
   public void stampaInfo(){
     System.out.println("serbatoio (" + serbatoio + "), " + ruote.stampaInfo());
+    System.out.println();
   }
+  
+  // METODO CHE RESTITUISCE LE PROPRIETA' DEI PNEUMATICI MONTATI
+  public Wheels getRuote() { 
+	  return ruote; 
+  }
+
+  // Aggiungo i metodi 'get' e 'set' mancanti
+  public String getNome() {
+		return nome;
+  }
+
+  public void setNome(String nome) {
+		this.nome = nome;
+  }
+
+  public float getResa() {
+		return resa;
+  }
+
+  public void setResa(float resa) {
+		this.resa = resa;
+  }
+
+  public float getMaxSerbatoio() {
+		return maxSerbatoio;
+  }  
 
 }
