c22e15ce17e28924625ee80555e0a320aed79761
ImLearningJava <85734828+ImLearningJava@users.noreply.github.com>
2021-06-15 15:03:22 +0200

    Update DistributoreBenzina.java

--- DistributoreBenzina.java
+++ DistributoreBenzina.java
@@ -1,3 +1,7 @@
+/***********************************************
+*	Paolo Bondi - Corso TIC (UF6) - 04/06/21   *
+*	Esercizio2 - Automobili e distributori v2  *											
+***********************************************/
 
 public class DistributoreBenzina {
   
@@ -18,9 +22,76 @@ public class DistributoreBenzina {
   }
 
   // TODO: vendi deve modificare anche il serbatorio dell'auto che sta rifornendo
-  // quindi deve prendere in input euro ma anche un oggetto Car a cui farÃ  rifornimento di gas in base all'importo pagato
-  public void vendi(double euro) {
-    deposito = deposito - (euro / euroPerLitro);
+  // quindi deve prendere in input euro ma anche un oggetto Car a cui farà rifornimento di gas in base all'importo pagato
+  // FATTO
+  
+  public float vendi(int euro, Car c) {
+	float quantitaAcquistabile = (float) euro/(float) euroPerLitro;      				// >>> nuovo
+	float quantitaAcquistabileBkp = quantitaAcquistabile;								// >>> nuovo
+	float serbatoioDisponibileBkp = c.serbatoioDisponibile();							// >>> nuovo
+	double depositoBkp = deposito;														// >>> nuovo
+	int tipo = 0;
+	float r = 0.0f;
+	
+	// Sono stati aggiunti i controlli sia sul serbatoio che sul deposito
+	// Il resto viene calcolato quando il serbatoio può contenere meno benzina di quella acquistabile con l'importo caricato
+	// e quando il distributore ha meno benzina di quella acquistabile
+	
+	if ((quantitaAcquistabile <= c.serbatoioDisponibile()) && (quantitaAcquistabile <= deposito))
+	{
+		tipo = 1;
+		deposito = deposito - (quantitaAcquistabile);
+	    c.setGas(quantitaAcquistabile);
+	    // r = 0.0f;
+	}
+	
+	else if ((quantitaAcquistabile <= c.serbatoioDisponibile()) && (quantitaAcquistabile > deposito))
+	{
+		tipo = 2;
+		quantitaAcquistabile = (float) deposito;
+		deposito = deposito - (quantitaAcquistabile);		// il deposito si azzera
+	    c.setGas(quantitaAcquistabile);
+	    r = (float)(quantitaAcquistabileBkp - quantitaAcquistabile)*(float)euroPerLitro;
+	}
+	
+	else if ((quantitaAcquistabile > c.serbatoioDisponibile()) && (c.serbatoioDisponibile() >= deposito))
+	{
+		tipo = 3;
+		quantitaAcquistabile = (float) deposito;
+		deposito = deposito - (quantitaAcquistabile);		// il deposito si azzera
+	    c.setGas(quantitaAcquistabile);
+	    r = (float)(quantitaAcquistabileBkp - quantitaAcquistabile)*(float)euroPerLitro;
+	}
+	
+	else if ((quantitaAcquistabile > c.serbatoioDisponibile()) && (c.serbatoioDisponibile() < deposito))
+	{
+		tipo = 4;
+		quantitaAcquistabile = c.serbatoioDisponibile();
+		deposito = deposito - (quantitaAcquistabile);		
+	    c.setGas(quantitaAcquistabile);
+	    r = (float)(quantitaAcquistabileBkp - quantitaAcquistabile)*(float)euroPerLitro;
+	}
+	
+	
+	/*             TABELLA DI VERIFICA CALCOLI
+		
+	System.out.println();
+	System.out.println("-------------------------------------------------------------------");
+	System.out.println("Tipo                    : " + tipo);
+	System.out.println("importo da spendere     : " + euro);
+	System.out.println("Quantità aqu. teorica   : " + quantitaAcquistabileBkp + " - " + quantitaAcquistabileBkp*euroPerLitro);
+	System.out.println("Quantità aqu. effettiva : " + quantitaAcquistabile + " - " + quantitaAcquistabile*euroPerLitro);
+	System.out.println("Serbatoio disp (prima)  : " + serbatoioDisponibileBkp + " - " + serbatoioDisponibileBkp*euroPerLitro);
+	System.out.println("Serbatoio disp (dopo)   : " + c.serbatoioDisponibile() + " - " + c.serbatoioDisponibile()*euroPerLitro);
+	System.out.println("Deposito (prima)        : " + depositoBkp + " - " + depositoBkp*euroPerLitro);
+	System.out.println("Deposito (dopo)         : " + deposito + " - " + deposito*euroPerLitro);
+	System.out.println("Resto                   : " + r);
+	System.out.println("-------------------------------------------------------------------");
+	System.out.println();
+	
+	*/
+		
+	return  r;
   }
 
   public void aggiorna(double unPrezzoPerLitro) {
@@ -31,16 +102,31 @@ public class DistributoreBenzina {
     return deposito;
   } 
   
-  
   // TODO: implementare un metodo che prende in input un oggetto Wheels e
-  //   fornisce la manutenzione necessaria (controlla le ruote e riporta lo
-  //   statoRuote a 0, controlla la pressione e riporta la pressione al massimo)
+  // fornisce la manutenzione necessaria (controlla le ruote e riporta lo
+  // statoRuote a 0, controlla la pressione e riporta la pressione al massimo)
+  // FATTO
+  
   public void manutenzioneRuote(Wheels ruote){
     // TODO implementare...
-    return;
+	// FATTO  
+	// ruote.manutenzioneRuote();	  
+	   
+	// definisco una variabile per scegliere la manutenzione da effettuare
+	int checkRuote = ruote.controlloRuote();
+	
+	if (checkRuote == 1 || checkRuote == 3) {
+		ruote.setAzzeraRuote();
+	}
+	  
+	if (checkRuote == 2 || checkRuote == 3) {
+		ruote.setMaxPressioneRuote();
+	} 
+	
+	return;
+	 	  
   }  
-  
-  
+    
   // metodi getter e setter 
   public double getDeposito() {
     return deposito;
