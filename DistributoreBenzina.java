aace30e373c0ee2bef457e74a467deae6eab125b
Matte961000 <85739961+Matte961000@users.noreply.github.com>
2021-06-15 15:53:27 +0200

    LavoroVenturi

--- DistributoreBenzina.java
+++ DistributoreBenzina.java
@@ -1,33 +1,49 @@
-
 public class DistributoreBenzina {
   
   // variabili di istanza della classe
   public double deposito;
-  public double euroPerLitro;
+  public float euroPerLitro;
   
   //  costruttore della classe
-  public DistributoreBenzina(double unPrezzoPerLitro){
-    setEuroPerLitro(unPrezzoPerLitro);
+  public DistributoreBenzina(double unPrezzoPerLitro)
+  {
+    setEuroPerLitro((float)unPrezzoPerLitro);
     setDeposito(0);
   }
   
   
   //  metodi della classe
-  public void rifornisci(double unaQuantita) {
+  public void rifornisci(double unaQuantita)
+  {
     deposito = deposito + unaQuantita;
   }
 
-  // TODO: vendi deve modificare anche il serbatorio dell'auto che sta rifornendo
-  // quindi deve prendere in input euro ma anche un oggetto Car a cui far√† rifornimento di gas in base all'importo pagato
-  public void vendi(double euro) {
-    deposito = deposito - (euro / euroPerLitro);
+  // TODO: vendi deve modificare anche il serbatorio dell'auto che sta rifornendo 
+  // quindi deve prendere in input euro ma anche un oggetto Car a cui far‡ rifornimento di gas in base all'importo pagato
+  //======FATTO======
+  public float vendi(float euro, Car mac)
+  {
+	  if(mac.getMax() < mac.getGas() + (euro / euroPerLitro))
+	  {
+		  float benzinaNonInseribile = mac.getGas() + (euro / euroPerLitro) - mac.getMax();
+		  deposito = deposito - (euro / euroPerLitro) + benzinaNonInseribile;
+		  mac.setGas((euro / euroPerLitro) - benzinaNonInseribile);
+		  
+		  return benzinaNonInseribile * euroPerLitro;
+	  }
+	  deposito = deposito - (euro / euroPerLitro);
+	  mac.setGas((euro / euroPerLitro));
+		
+	  return 0;
   }
 
-  public void aggiorna(double unPrezzoPerLitro) {
+  public void aggiorna(float unPrezzoPerLitro)
+  {
     euroPerLitro = unPrezzoPerLitro;
   }
 
-  public double getBenzina() {
+  public double getBenzina()
+  {
     return deposito;
   } 
   
@@ -35,26 +51,55 @@ public class DistributoreBenzina {
   // TODO: implementare un metodo che prende in input un oggetto Wheels e
   //   fornisce la manutenzione necessaria (controlla le ruote e riporta lo
   //   statoRuote a 0, controlla la pressione e riporta la pressione al massimo)
-  public void manutenzioneRuote(Wheels ruote){
-    // TODO implementare...
-    return;
+  //======FATTO======
+  public void manutenzioneRuote(Wheels ruote)
+  {
+	  if (ruote.controlloRuote() == 1)
+	  {
+		  //System.out.println("Necessario controllare le ruote");
+		  ruote.resetStatoRuote();
+		  return;
+	  }
+	  	
+	  if (ruote.controlloRuote() == 2)
+	  {
+		  //System.out.println("Necessario gonfiare le ruote");
+		  ruote.resetPressioneRuote();
+	  	  return;
+	  }
+	  	
+	  if (ruote.controlloRuote() == 3)
+	  {
+	  	  //System.out.println("Necessario controllare e gonfiare le ruote");
+	  	  ruote.resetStatoRuote();
+	  	  ruote.resetPressioneRuote();
+	  	  return;
+	  }
+	  	
+	  //System.out.println("Ruote in perfetto stato, nessuna operazione richiesta");
+	  return;
   }  
   
   
   // metodi getter e setter 
-  public double getDeposito() {
+  public double getDeposito() 
+  {
     return deposito;
   }
-  public void setDeposito(double deposito) {
+  
+  public void setDeposito(double deposito)
+  {
     this.deposito = deposito;
   }
-  public double getEuroPerLitro() {
+  
+  public double getEuroPerLitro()
+  {
     return euroPerLitro;
   }
-  public void setEuroPerLitro(double euroPerLitro) {
+  
+  public void setEuroPerLitro(float euroPerLitro)
+  {
     this.euroPerLitro = euroPerLitro;
   }
   
-  
-
 }
--- Wheels.java
+++ Wheels.java
@@ -3,41 +3,71 @@ public class Wheels {
   private float statoRuote; // km percorsi per valutare l'usura delle ruote
   private float pressioneRuote;
   
-  public Wheels(float maxPressione) {
+  public Wheels(float maxPressione)
+  {
     statoRuote = 0;
     pressioneRuote = maxPressione; // inizializzo ruote gonfie al massimo, 2.5 bar
   }
   
-  public float getStatoRuote() { return statoRuote; }
-  public float getPressioneRuote() { return pressioneRuote; }
+  public float getStatoRuote()
+  {
+	  return statoRuote;
+  }
+  public float getPressioneRuote()
+  {
+	  return pressioneRuote;
+  }
   
-  public void setStatoRuote(float percorso) { statoRuote += percorso; }
-  public void setPressioneRuote(float percorso) { 
+  public void resetStatoRuote()
+  {
+	  statoRuote = 0;
+  }
+  
+  public void resetPressioneRuote()
+  {
+	  pressioneRuote = 2.5f;
+  }
+ 
+  public void setStatoRuote(float percorso)
+  {
+	  statoRuote += percorso;
+  }
+  
+  public void setPressioneRuote(float percorso)
+  { 
     pressioneRuote -= 0.1f * (percorso/50);
     if (pressioneRuote < 0.2f)
       pressioneRuote = 0.01f; 
   }
     
-  public void controlloRuote(){
+  public int controlloRuote()
+  {
   
-  	if (statoRuote > 300 && pressioneRuote < 1.5) {
+  	if (statoRuote > 300 && pressioneRuote < 1.5)
+  	{
   	  System.out.println("Necessario controllare e gonfiare le ruote");
-  	  return;
+  	  return 3;
   	}
   	
-  	if (statoRuote > 300) {
+  	if (statoRuote > 300)
+  	{
   	  System.out.println("Necessario controllare le ruote");
-  	  return;
+  	  return 1;
   	}
   	
-  	if (pressioneRuote < 1.5){
+  	if (pressioneRuote < 1.5)
+  	{
   	  System.out.println("Necessario gonfiare le ruote");
-  	  return;
+  	  return 2;
   	}
   	
   	System.out.println("Ruote in perfetto stato, nessuna operazione richiesta");
+  	return 0;
   }
   
-  public String stampaInfo() { return "statoRuote (" + statoRuote + "), pressioneRuote (" + pressioneRuote + ")"; }
+  public String stampaInfo()
+  {
+	  return "statoRuote (" + statoRuote + "), pressioneRuote (" + pressioneRuote + ")";
+  }
   
 }
